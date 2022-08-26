50616cab4ca443e5d58f47e6eb9ddfe6f3c70aa5
MooNRakeR <moonraker@MooNRakeRs-MacBook-Pro.local>
2022-08-26 18:37:19 -0500

    particles

---
+++ 18-particles/18-vite/main.js
@@ -0,0 +1,166 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'lil-gui'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('/textures/particles/2.png')
+
+/**
+ * Particles
+ */
+// Geometry
+// const particlesGeometry = new THREE.SphereGeometry(1, 32, 32)
+// Geometry
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 50000
+
+const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
+
+for (let i = 0; i < count * 3; i++) {
+  positions[i] = (Math.random() - 0.5) * 10
+  colors[i] = Math.random()
+}
+
+particlesGeometry.setAttribute(
+  'position',
+  new THREE.BufferAttribute(positions, 3)
+)
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3)) // Create the Three.js BufferAttribute and specify that each information is composed of 3 values
+// Material
+const particlesMaterial = new THREE.PointsMaterial({
+  size: 0.03,
+  sizeAttenuation: true,
+  // transparent: true,
+  // depthWrite: false,
+  // blending: THREE.AdditiveBlending,
+  // vertexColors: true,
+})
+// particlesMaterial.map = particleTexture
+// particlesMaterial.map = particleTexture
+
+// fix blocking particles
+particlesMaterial.transparent = true
+particlesMaterial.alphaMap = particleTexture
+particlesMaterial.vertexColors = true
+// particlesMaterial.color = new THREE.Color('#ff88cc')
+// particlesMaterial.alphaTest = 0.001
+// particlesMaterial.depthTest = false
+particlesMaterial.depthWrite = false
+particlesMaterial.blending = THREE.AdditiveBlending
+
+// const particlesMaterial = new THREE.PointsMaterial()
+// particlesMaterial.size = 0.02
+// particlesMaterial.sizeAttenuation = true
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
+// const cube = new THREE.Mesh(
+//   new THREE.BoxGeometry(),
+//   new THREE.MeshBasicMaterial()
+// )
+// scene.add(cube)
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+
+  // Update controls
+  controls.update()
+  // Animate
+  // particles.rotation.y = elapsedTime * 0.2
+  // Or Wave
+
+  // for (let i = 0; i < count; i++) {
+  //   const i3 = i * 3
+
+  //   particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime)
+  // }
+  // particlesGeometry.attributes.position.needsUpdate = true
+
+  // Or
+  for (let i = 0; i < count; i++) {
+    let i3 = i * 3
+
+    const x = particlesGeometry.attributes.position.array[i3]
+    particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(
+      elapsedTime + x
+    )
+  }
+  particlesGeometry.attributes.position.needsUpdate = true
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
+}
+
+tick()
